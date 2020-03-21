175adccd03b93b174e05f7a06e46fb772dbd77e8
Gaspar de Elias <caspercba@gmail.com>
2020-03-21 18:13:18 -0300

    Initial project structure

---
+++ app/src/main/java/com/gaspardeelias/quickreddit/application/ApiModule.kt
@@ -0,0 +1,12 @@
+package com.gaspardeelias.quickreddit.application
+
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class ApiModule {
+    @Provides
+    @Singleton
+    
+}
